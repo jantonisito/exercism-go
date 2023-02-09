effdf5a054dc63588325c0204f2d7184601ba178
Tom Niesytto <tom_niesytto@yahoo.com>
2023-02-09 16:05:05 -0600

    Iteration #3
    Removed WaitGroup related code

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,7 +1,5 @@
 package letter
 
-//import "sync"
-
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -37,63 +35,3 @@ func ConcurrentFrequency(l []string) FreqMap {
 	}
 	return out
 }
-
-// // ConcurrentFrequency counts the frequency of each rune in the given strings, by making use of concurrency.
-// func ConcurrentFrequency(l []string) FreqMap {
-// 	var wg sync.WaitGroup
-
-// 	c := make(chan *FreqMap, len(l))
-// 	fm := make([]*FreqMap, len(l))
-
-// 	for i, s := range l {
-// 		wg.Add(1)
-// 		go func(s string) {
-// 			defer wg.Done()
-// 			x := Frequency(s)
-// 			c <- &x
-// 		}(s)
-// 		// This causes the channel to block each iteration, which means that
-// 		// the sending goroutines are blocked until a receiving goroutine reads
-// 		// from the channel (see: ConcurrentFrequency2). Usage of WaitGroup in
-// 		// this case is unnecessary since reading from channel will block anyway.
-// 		fm[i] = <-c
-// 	}
-// 	wg.Wait()
-
-// 	out := FreqMap{}
-// 	for _, f := range fm {
-// 		for k, v := range *f {
-// 			out[k] += v
-// 		}
-// 	}
-// 	return out
-// }
-
-// // ConcurrentFrequency counts the frequency of each rune in the given strings, by making use of concurrency.
-// func ConcurrentFrequency(l []string) FreqMap {
-// 	var wg sync.WaitGroup
-
-// 	c := make(chan *FreqMap, len(l))
-
-// 	for i := range l {
-// 		wg.Add(1)
-// 		go func(i int) {
-// 			defer wg.Done()
-// 			x := Frequency(l[i])
-// 			c <- &x
-// 		}(i)
-// 		// Note: we only write to channel - there is no blocking
-// 	}
-// 	// Blocking is done by WaitGroup - which is necessary here
-// 	wg.Wait()
-
-// 	out := make(FreqMap, 48)
-// 	for range l {
-// 		//we defer reading from channel until all work is done
-// 		f := <-c
-// 		for k, v := range *f {
-// 			out[k] += v
-// 		}
-// 	}
-// 	return out
-// }
