3e8014c19fd886825cce9df11b180d52b6f7b8aa
Jan Tonisito <tom_niesytto@yahoo.com>
2023-02-26 15:01:20 -0600

    Iteration #7

---
+++ bob/bob_test.go
@@ -0,0 +1,29 @@
+package bob
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHey(t *testing.T) {
+	for _, tt := range testCases {
+		fmt.Println(tt.description)
+		actual := Hey(tt.input)
+		if actual != tt.expected {
+			msg := `
+	ALICE (%s): %q
+	BOB: %s
+
+	Expected Bob to respond: %s`
+			t.Fatalf(msg, tt.description, tt.input, actual, tt.expected)
+		}
+	}
+}
+
+func BenchmarkHey(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range testCases {
+			Hey(tt.input)
+		}
+	}
+}
