9679593ada9856cb7e87f36a3286bd66900973f8
Jan Tonisito <tom_niesytto@yahoo.com>
2025-06-12 03:41:25 -0500

    Initial commit

---
+++ raindrops/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	raindrops "exercism/Go/raindrops/code"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(raindrops.Convert(37))
+	fmt.Println(raindrops.Convert(73))	
+
+	fmt.Println(raindrops.Convert(3*5*7))	
+	fmt.Println(raindrops.Convert(7))	
+}
